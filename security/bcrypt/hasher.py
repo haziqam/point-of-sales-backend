0e9d7d77373106750de65365d5c0aee7b1937721
Haziq <haziqabiyyu@gmail.com>
2024-01-06 09:53:51 +0700

    refactor: move security outside of core

--- security/bcrypt/hasher.py
+++ security/bcrypt/hasher.py
@@ -1,11 +1,11 @@
-from core.security.hasher import PasswordHasher
+from security.hasher import IPasswordHasher
 
 import bcrypt
 
-class BCryptPasswordHasher(PasswordHasher):
+class BCryptPasswordHasher(IPasswordHasher):
     def hash(self, password: str) -> str:
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         return hashed_password.decode('utf-8')
 
-    def verify_password(self, entered_password: str, stored_password: str) -> bool:
+    def verify(self, entered_password: str, stored_password: str) -> bool:
         return bcrypt.checkpw(entered_password.encode('utf-8'), stored_password.encode('utf-8'))
